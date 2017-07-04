8527bbd796416bcd4c829e4fc3186fb734b8d771
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-04 20:31:41 +0530

    Create Armstrongcheck.java

---
+++ Armstrongcheck.java
@@ -0,0 +1,21 @@
+# set2
+   import java.util.*;
+import java.io.*;
+public class Armstrongcheck{  
+  public static void main(String[] args)  {  
+    Scanner sc=new Scanner(System.in);  
+    int c=0,a,temp;  
+    int n=sc.nextInt(); 
+    temp=n;  
+    while(n>0)  
+    {  
+      a=n%10;  
+      n=n/10;  
+      c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("armstrong number");   
+    else  
+    System.out.println("Not armstrong number");   
+  }  
+}  
