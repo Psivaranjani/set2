a71f3807b53ddcf98a3355fc559dfdbb630f93be
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-04 18:24:17 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,30 @@
+# set2
+import java.util.*;
+import java.io.*;
+public class Prime{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the starting number");
+int a=sc.nextInt();
+System.out.println("Enter the ending number");
+int b=sc.nextInt();
+System.out.println("The prime numbers are:");
+int flag=0;
+for(int i=a;i<b;;i++)
+{
+for(int j=2;j<i;j++)
+{
+if(i%j==0)
+{
+flag=0;
+break;
+}
+else{
+flag=1;
+}
+}
+if(flag==1)
+System.out.println(i);
+}
+}
+}
