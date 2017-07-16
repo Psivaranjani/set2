de14231a663e765d17442010887e17702040625e
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-16 14:13:34 +0530

    Create NumSum.java

---
+++ NumSum.java
@@ -0,0 +1,18 @@
+# set2
+import java.io.*;
+import java.util.*;
+public class NumSum{
+  public static void main(String args[]){
+    int a=0,b=0;
+    for(int i=1;i<=15;i++)
+    {
+      a+=i;
+      }
+      System.out.println(a);
+      for(int j=15;j<=45;j+=2)
+      {
+        b+=j;
+        }
+        System.out.println(b);
+        }
+}
